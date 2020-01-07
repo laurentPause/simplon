38f4b694c15f925ce487aa40b3e761eb00b97bda
laurentPause <48903538+laurentPause@users.noreply.github.com>
2020-01-07 14:36:59 +0400

    Ajout MVC et config de base
    Fichier MVC de base
    Conf site et database
    Retirer index.php dans url avec apache

---
+++ application/controllers/User.php
@@ -0,0 +1,16 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class User extends CI_Controller {
+
+	public function __construct()
+    {
+		parent::__construct();
+		
+        $this->load->model('user_model', 'user');
+    }
+	public function index()
+	{
+		$this->load->view('login');
+	}
+}
---
+++ application/models/Attribuer_model.php
@@ -0,0 +1,10 @@
+<?php
+class Attribuer_model extends CI_Model {
+
+    public function __construct()
+    {
+        parent::__construct();
+
+        $this->attribuer = 'attribuer';
+    }
+}
